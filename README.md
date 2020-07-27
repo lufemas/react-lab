7c5e7ef33666801c420d75059328e4614fdabb5d
JR Maschietto <maschietto.jr@gmail.com>
2020-07-27 19:18:43 -0400

    README with LIVE PAGE

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # React-Lab Project
 
+[LIVE PAGE](https://react-lab-jb9iw37vg.vercel.app/)
+
 - Product Listing
 - useState, props, react-router-dom
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
 	entry : ['react-hot-loader/patch','./src/index.js'],
 	output:{
-		path: path.join(__dirname,'/dist'),
+		path: path.join(__dirname,'/public'),
 		filename: 'main.js',
 	},
 	module: {
